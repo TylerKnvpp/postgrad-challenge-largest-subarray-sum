e5d80d3938593aed67297b11a8a562ab7592c9cd
Tyler Knapp <tgknapp11@gmail.com>
2020-04-14 11:48:49 -0700

    6/9 passing

---
+++ index.js
@@ -0,0 +1,24 @@
+let arr = [1, -1, 5, 3, -7, 4, 5, 6, -100, 4];
+
+function largestSubarraySum(array) {
+  let results = {
+    trackingSum: 0,
+    largestSum: 0,
+    numsToAdd: [],
+  };
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] + results.trackingSum > results.trackingSum) {
+      results.trackingSum += array[i];
+      results.largestSum += array[i];
+    }
+    // if (array[i] + array[i + 1] > array[i - 1]) {
+    //   results.trackingSum += array[i];
+    //   results.largestSum += array[i];
+    // }
+  }
+
+  return results.largestSum;
+}
+
+console.log(largestSubarraySum(arr));
